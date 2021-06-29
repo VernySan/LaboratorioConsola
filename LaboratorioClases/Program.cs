6bd197d39aa4c16a26f720ce04d1d6cc5f673577
verny <verny.mata@icumed.com>
2021-06-29 09:35:39 -0600

    Add project files.

---
+++ LaboratorioClases/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LaboratorioClases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
